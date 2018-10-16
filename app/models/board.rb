e15d9a44e9ab1df0c70bdc5df9d816ed2fa45c69
sugawara <pgjsugawara258@icloud.com>
2018-10-17 08:15:03 +0900

    Add board function

---
+++ app/models/board.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  name       :string           not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Board < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015021428) do
+ActiveRecord::Schema.define(version: 20181015055228) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
