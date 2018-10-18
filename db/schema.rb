6fab57542feefd727dcfaaf051496b1854d00f1e
sugawara <pgjsugawara258@icloud.com>
2018-10-18 14:03:54 +0900

    Add tag function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181017030853) do
+ActiveRecord::Schema.define(version: 20181017230712) do
+
+  create_table "board_tag_relations", force: :cascade do |t|
+    t.integer "board_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_board_tag_relations_on_board_id"
+    t.index ["tag_id"], name: "index_board_tag_relations_on_tag_id"
+  end
 
   create_table "boards", force: :cascade do |t|
     t.string "name", null: false
@@ -27,6 +36,12 @@ ActiveRecord::Schema.define(version: 20181017030853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
